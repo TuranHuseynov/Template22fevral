c9f587dac4ce75e9b09ed7eed2f1c507c2f72d0f
TuranHuseynov <33083619+TuranHuseynov@users.noreply.github.com>
2018-02-22 09:38:16 +0400

    22fevralTemplateEnd

---
+++ MyNewTemplate/Controllers/LoginController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyNewTemplate.Models;
+
+namespace MyNewTemplate.Controllers
+{
+    public class LoginController : Controller
+    {
+        newDataEntities db = new newDataEntities();
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        
+
+        [HttpGet]
+        public ActionResult LogIn()
+        {
+            return View();
+        }
+
+
+
+
+        [HttpPost]
+        public ActionResult LogIn(FormCollection frm)
+        {
+            Session["User_email"] = frm["User_email"];
+            Session["User_password"] = frm["User_password"];
+
+            if(Session["User_email"].ToString() == "admin@mail.ru")
+            {
+                if(Session["User_password"].ToString() == "admin")
+                {
+                    return RedirectToAction("Index", "Admin");
+                }
+                else
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            else
+            {
+                RedirectToAction("Index");
+            }
+
+            return View();
+        }
+      
+
+
+
+        public ActionResult LogOut()
+        {
+            Session.Abandon();
+            return RedirectToAction("Index", "LogIn");
+        }
+
+        
+        private bool Check_Admin()
+        {
+            if (Session["User_email"] != null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        private bool CheckEmp()
+        {
+
+            if (Session["Emp_user"] != null)
+            {
+                ViewBag.message = "Xosgelmisiniz " + Session["Emp_user"];
+                return true;
+            }
+            else
+            {
+                ViewBag.message = "Basqa sehifeye yonlendirilirsiniz...";
+                return false;
+            }
+        }
+
+    }
+}
+
+
+
+
+
+
+
+           
---
+++ MyNewTemplate/Controllers/UserLogController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyNewTemplate.Models;
+namespace MyNewTemplate.Controllers
+{
+    public class UserLogController : Controller
+    {
+        newDataEntities db = new newDataEntities();
+        // GET: UserLog
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public ActionResult UsrLog()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UsrLog(Employee emp)
+        {
+
+            Employee logEmp = db.Employees.Where(e => e.emp_email == emp.emp_email).First();
+            Session["Emp_email"] = emp.emp_email;
+            Session["Emp_password"] = emp.emp_password;
+            
+            if (logEmp.emp_email == emp.emp_email)
+            {
+                if (logEmp.emp_password == emp.emp_password)
+                {
+                    Session["Emp_email"] = logEmp.emp_email;
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    return RedirectToAction("Index");
+                }
+
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult LogOut()
+        {
+            Session.Abandon();
+            return RedirectToAction("Index", "UserLog");
+        }
+
+
+    }
+}
